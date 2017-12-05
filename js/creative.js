351e7e79b515bd90ea77a6dc56cca57c1f156085
Nate Foster <jnfoster@cs.cornell.edu>
2017-12-04 21:40:12 -0500

    Small fixes to HTML, JS, and CSS

--- js/creative.js
+++ js/creative.js
@@ -17,10 +17,10 @@
     });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
+    //$('body').scrollspy({
+    //    target: '.navbar-fixed-top',
+    //    offset: 51
+    //})
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -36,11 +36,11 @@
     );
 
     // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+    //$('#mainNav').affix({
+    //    offset: {
+    //        top: 100
+    //    }
+    //})
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
--- js/new-age.js
+++ js/new-age.js
@@ -10,16 +10,16 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 100
-    });
+    // // Highlight the top nav as scrolling occurs
+    // $('body').scrollspy({
+    //     target: '.navbar-fixed-top',
+    //     offset: 100
+    // });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+    // // Closes the Responsive Menu on Menu Item Click
+    // $('.navbar-collapse ul li a').click(function() {
+    //     $('.navbar-toggle:visible').click();
+    // });
 
     // Offset for Main Navigation
     $('#mainNav').affix({
